27733fd5ce8aca3b8734c65a0375fe9fbc947dec
Marko Bencun <mbencun@gmail.com>
2013-05-22 15:57:23 +0200

    GP -> BG

--- GAuth.hs
+++ GAuth.hs
@@ -2,7 +2,7 @@ module GAuth(gauth_hotp, gauth_totp) where
 
 import Data.HMAC (hmac_sha1)
 import qualified Data.Binary.Put as BP
-import qualified Data.Binary.Get as GP
+import qualified Data.Binary.Get as BG
 import qualified Data.ByteString.Lazy as BS
 import qualified Codec.Binary.Base32 as B32
 import Data.Time.Clock.POSIX(getPOSIXTime)
@@ -18,7 +18,7 @@ gauth_hotp secret intervalNo = printf "%06i" token
     hash = hmac_sha1 key $ BS.unpack msg
     last_nibble = fromIntegral $ last hash .&. 0x0f
     hash_part = take 4 $ drop last_nibble hash
-    token_base = (GP.runGet GP.getWord32be $ BS.pack $ hash_part) .&. 0x7fffffff
+    token_base = (BG.runGet BG.getWord32be $ BS.pack $ hash_part) .&. 0x7fffffff
     token = token_base `rem` 1000000
 
 gauth_totp :: String -> IO (String, Int)
